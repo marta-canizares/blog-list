11bdd21b0b91307b0ccad52da25cc9420af7dd0e
marta-canizares <marta.s.canizares@gmail.com>
2020-06-09 12:33:56 +0200

    ok

--- model/dao.js
+++ model/dao.js
@@ -1,32 +1,25 @@
-import connection from '../mongo/mongo.js'
-import Blog from './model.js';
+import connection from '../../mongo/mongo.js';
+import Blog from './model.js'
 import { response } from 'express';
-
 class BlogDAO {
-
     constructor() {
-
     }
-
     list() {
-        return Blog.find({})
+        return Blog
+            .find({});
     }
-
     create(pBlog) {
         const blog = new Blog({
             title: pBlog.title,
             author: pBlog.author,
             url: pBlog.url,
-            likes: pBlog.likes,
+            likes: pBlog.likes
         })
-
-        blog.save()
+        blog
+            .save()
             .then(result => {
-                response.status(201).json(result);
+                response.status(201).json(result)
             })
     }
-
 }
-
 export default new BlogDAO();
-
